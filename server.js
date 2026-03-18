6f3572e928e40be1fbe82e10f3db259310ba4cca
klimsporyshevklim-collab <klim.sporyshev.klim@gmail.com>
2026-03-18 20:49:49 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
 
+app.use(cors()); // Разрешаем всем (включая Firebase) брать файлы
+
+// Раздаем статические файлы из папки public
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.send('Scarab Engine Asset Server is Running!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
